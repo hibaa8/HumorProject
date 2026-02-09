d4490fae30fa479bce9c29ed6ae510743a7f78ac
hibaa8 <hibaaltaf98@gmail.com>
2026-02-09 01:47:19 -0500

    update page size limit

--- src/app/CaptionBrowser.tsx
+++ src/app/CaptionBrowser.tsx
@@ -19,7 +19,7 @@ type CaptionRow = {
   } | null;
 };
 
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 20;
 
 export default function CaptionBrowser() {
   const [flavors, setFlavors] = useState<HumorFlavor[]>([]);
